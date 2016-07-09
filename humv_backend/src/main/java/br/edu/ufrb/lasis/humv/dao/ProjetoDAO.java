a7dba2a1788d430347b8ce228929dec0e870be2d
Vinicius Moura <viniciusmq@outlook.com>
2016-07-08 23:06:50 -0300

    Porojeto Implementado

---
+++ humv_backend/src/main/java/br/edu/ufrb/lasis/humv/dao/ProjetoDAO.java
@@ -0,0 +1,85 @@
+package br.edu.ufrb.lasis.humv.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import br.edu.ufrb.lasis.humv.entity.Projeto;
+
+@Repository
+public class ProjetoDAO extends GenericDAO<Projeto> implements Serializable {
+
+private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	
+	
+	/**
+	 * Saves an project in the database.
+	 *
+	 * @param projeto
+	 *            the Project to be saved
+	 */
+	@Transactional
+	public void saveProjeto(Projeto projeto) {
+		super.save(projeto);
+	}
+
+	/**
+	 * Updates an project in the database.
+	 *
+	 * @param projeto
+	 *            the Project to be updated
+	 */
+	@Transactional
+	public void updateProjeto(Projeto projeto) {
+		super.update(projeto);
+	}
+	
+	/**
+	 * Removes an project in the database.
+	 *
+	 * @param projeto
+	 *            the Project to be removed
+	 */
+	@Transactional
+	public void removeProjeto(Projeto projeto) {
+		super.delete(projeto);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Projeto> findBySiapeResponsavel(int siape){
+		Criteria criteria = getCriteria().add(Restrictions.ilike("siapeResponsavel", "%" + siape + "%"));
+		criteria.addOrder(Order.asc("siapeResponsavel"));
+		return (List<Projeto>) criteria.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Projeto> findByNome(String nome){
+		Criteria criteria = getCriteria().add(Restrictions.ilike("nome", "%" + nome + "%"));
+		criteria.addOrder(Order.asc("nome"));
+		return (List<Projeto>) criteria.list();
+	}
+	
+	@Transactional
+	public Projeto findByKey(String id) {
+		return (Projeto) getCriteria().add(Restrictions.eq("id", id)).uniqueResult();
+	}
+
+}
---
+++ humv_backend/src/main/java/br/edu/ufrb/lasis/humv/impl/ProjetoServiceImpl.java
@@ -0,0 +1,74 @@
+package br.edu.ufrb.lasis.humv.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import br.edu.ufrb.lasis.humv.dao.ProjetoDAO;
+import br.edu.ufrb.lasis.humv.entity.Projeto;
+
+@Service
+public class ProjetoServiceImpl {
+	
+	private final static Logger logger = LoggerFactory.getLogger(ProjetoServiceImpl.class);
+	
+	
+@Autowired
+private ProjetoDAO projetoDAO ;
+
+
+public List<Projeto> getAll(){
+	try {
+		return projetoDAO.findAll();
+	} catch (Exception e) {
+		e.printStackTrace();
+		return new ArrayList<Projeto>();
+	}
+}
+
+public Projeto findById(String id){
+	return projetoDAO.findByKey(id);
+}
+
+public String cadastrarProjeto(Projeto projeto, String usuarioResponsavel){
+	try{
+		projetoDAO.saveProjeto(projeto);
+		logger.info("[signup - " + usuarioResponsavel + "] Projeto cadastrado com sucesso: " + projeto.getNome() + ".");
+		return "OK";
+	}catch(DataIntegrityViolationException ex){
+		if(ex.getMessage().toLowerCase().contains("constraint")){
+			logger.error("[signup] Projeto ja cadastrado: " + projeto.getNome() + ".");
+			return "Projeto com Nome " + projeto.getNome() + " ja cadastrado no sistema. Por favor, informe um Nome diferente.";
+		}else{
+			return "Erro ao conectar-se com o banco de dados.";
+		}
+	}
+}
+
+public String atualizarProjeto(Projeto projeto, String usuarioResponsavel){
+	if(projetoDAO.findBySiapeResponsavel(projeto.getSiapeResponsavel())==null){
+		logger.error("[signup] Nenhum projeto com o ID " + projeto.getId() + "foi encontrado no sistema.");
+		return "Nenhum projeto com o ID " + projeto.getId() + " encontrado no sistema. Por favor, informe um ID diferente.";
+	}
+	projetoDAO.updateProjeto(projeto);
+	logger.info("[atualizarProjeto - " + usuarioResponsavel + "] Projeto " + projeto.getNome() + " atualizado com sucesso.");
+	return "OK";
+}
+
+public String removerProjeto(String id, String usuarioResponsavel){
+	if(projetoDAO.findByKey(id)==null){
+		logger.error("[signup] Nenhum projeto com o ID " + id + "foi encontrado no sistema.");
+		return "Nenhum projeto com o ID " + id + " encontrado no sistema. Por favor, informe um siape diferente.";
+	}
+	Projeto projeto = projetoDAO.findByKey(id);
+	projetoDAO.removeProjeto(projeto);
+	logger.info("[removerProjeto - " + usuarioResponsavel + "] Projeto " + projeto.getNome() + " removido com sucesso.");
+	return "OK";
+}
+
+}
