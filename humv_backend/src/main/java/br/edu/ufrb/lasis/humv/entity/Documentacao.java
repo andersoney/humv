df68a375cde188af456dc27c763f7822cd57ff0b
andersoney <andersoney@gmail.com>
2016-10-20 20:00:09 -0300

    Revert "Colocada ID"
    This reverts commit b93c7f6524e345afe030af27d1f4adf9f9badfb4.

--- humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Documentacao.java
+++ humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Documentacao.java
@@ -5,54 +5,37 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
 public class Documentacao implements Serializable {
-
-    @Id
-    private Long ID;
-
-    private static final long serialVersionUID = -6125748157292589614L;
-    @Column
-    @Temporal(TemporalType.DATE)
-    private Date dataEntrega;
-    @Column
-    private String nomeRecebinte;
-    @Column
-    private String nomeDocumento;
-
-    public Date getDataEntrega() {
-        return dataEntrega;
-    }
-
-    public void setDataEntrega(Date dataEntrega) {
-        this.dataEntrega = dataEntrega;
-    }
-
-    public String getNomeRecebinte() {
-        return nomeRecebinte;
-    }
-
-    public void setNomeRecebinte(String nomeRecebinte) {
-        this.nomeRecebinte = nomeRecebinte;
-    }
-
-    public String getNomeDocumento() {
-        return nomeDocumento;
-    }
-
-    public void setNomeDocumento(String nomeDocumento) {
-        this.nomeDocumento = nomeDocumento;
-    }
-
-    public Long getID() {
-        return ID;
-    }
-
-    public void setID(Long ID) {
-        this.ID = ID;
-    }
+	
+	private static final long serialVersionUID = -6125748157292589614L;
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date dataEntrega;
+	@Column
+	private String nomeRecebinte;
+	@Column
+	private String nomeDocumento;
+	
+	public Date getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(Date dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public String getNomeRecebinte() {
+		return nomeRecebinte;
+	}
+	public void setNomeRecebinte(String nomeRecebinte) {
+		this.nomeRecebinte = nomeRecebinte;
+	}
+	public String getNomeDocumento() {
+		return nomeDocumento;
+	}
+	public void setNomeDocumento(String nomeDocumento) {
+		this.nomeDocumento = nomeDocumento;
+	}
 }
