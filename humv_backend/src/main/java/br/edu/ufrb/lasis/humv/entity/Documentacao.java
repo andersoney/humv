70800e69d161fe8a837031b0ed81f4e556ac3e1c
Luiz Toni <luiztoni@outlook.com>
2016-10-20 21:02:04 -0300

    #fix: Hibernate - collections mappings

--- humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Documentacao.java
+++ humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Documentacao.java
@@ -6,66 +6,15 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-<<<<<<< HEAD
-import javax.persistence.Id;
-=======
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
->>>>>>> 0674d9dd408ddd0da55809869c52e6aee58483cd
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="DOCUMENTACAO")
 public class Documentacao implements Serializable {
-<<<<<<< HEAD
-
-    @Id
-    private Long ID;
-
-    private static final long serialVersionUID = -6125748157292589614L;
-    @Column
-    @Temporal(TemporalType.DATE)
-    private Date dataEntrega;
-    @Column
-    private String nomeRecebinte;
-    @Column
-    private String nomeDocumento;
-
-    public Date getDataEntrega() {
-        return dataEntrega;
-    }
-
-    public void setDataEntrega(Date dataEntrega) {
-        this.dataEntrega = dataEntrega;
-    }
-
-    public String getNomeRecebinte() {
-        return nomeRecebinte;
-    }
-
-    public void setNomeRecebinte(String nomeRecebinte) {
-        this.nomeRecebinte = nomeRecebinte;
-    }
-
-    public String getNomeDocumento() {
-        return nomeDocumento;
-    }
-
-    public void setNomeDocumento(String nomeDocumento) {
-        this.nomeDocumento = nomeDocumento;
-    }
-
-    public Long getID() {
-        return ID;
-    }
-
-    public void setID(Long ID) {
-        this.ID = ID;
-    }
-}
-=======
 	
 	private static final long serialVersionUID = -6125748157292589611L;
 	@Id
@@ -99,4 +48,3 @@ public class Documentacao implements Serializable {
 		this.nomeDocumento = nomeDocumento;
 	}
 }
->>>>>>> 0674d9dd408ddd0da55809869c52e6aee58483cd
