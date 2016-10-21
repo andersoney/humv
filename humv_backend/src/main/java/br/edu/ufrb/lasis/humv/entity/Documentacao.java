08f94237b2daa28efe0708d00c7f2124a933827b
andersoney <andersoney@gmail.com>
2016-10-20 23:16:51 -0300

    Servidor Criado e configurado e chekado

--- humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Documentacao.java
+++ humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Documentacao.java
@@ -16,7 +16,9 @@ import javax.persistence.TemporalType;
 @Table(name="DOCUMENTACAO")
 public class Documentacao implements Serializable {
 	
-	private static final long serialVersionUID = -6125748157292589611L;
+
+	private static final long serialVersionUID = -6125748157292589614L;
+	
 	@Id
 	@GeneratedValue
 	private BigInteger id;
@@ -24,26 +26,41 @@ public class Documentacao implements Serializable {
 	@Column
 	@Temporal(TemporalType.DATE)
 	private Date dataEntrega;
+	
 	@Column
 	private String nomeRecebinte;
+	
 	@Column
 	private String nomeDocumento;
 	
+	public BigInteger getId() {
+		return id;
+	}
+
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+
 	public Date getDataEntrega() {
 		return dataEntrega;
 	}
+	
 	public void setDataEntrega(Date dataEntrega) {
 		this.dataEntrega = dataEntrega;
 	}
+	
 	public String getNomeRecebinte() {
 		return nomeRecebinte;
 	}
+	
 	public void setNomeRecebinte(String nomeRecebinte) {
 		this.nomeRecebinte = nomeRecebinte;
 	}
+	
 	public String getNomeDocumento() {
 		return nomeDocumento;
 	}
+	
 	public void setNomeDocumento(String nomeDocumento) {
 		this.nomeDocumento = nomeDocumento;
 	}
