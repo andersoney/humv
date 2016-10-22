c225e68d647098b0cb840402d88de1fb1b063b5f
andersoney <andersoney@gmail.com>
2016-10-22 20:18:49 -0300

    QuestionarioSocialTableModel Criada e construida, porem faltando a obtenção dos ArrayList e de elemento, assim como remoção e add

---
+++ humv_desktop/src/main/java/br/edu/ufrb/lasis/humv/view/questionario/QuestionarioSocialTableModel.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ufrb.lasis.humv.view.questionario;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import br.edu.ufrb.lasis.humv.entity.QuestionarioSocioeconomico;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Orion
+ */
+public class QuestionarioSocialTableModel extends AbstractTableModel {
+
+    String[] coluna = new String[]{"Nome do Dono", "CPF"};
+    ArrayList<QuestionarioSocioeconomico> questionarios;
+
+    public QuestionarioSocialTableModel() {
+        this.questionarios = new ArrayList<QuestionarioSocioeconomico>();
+    }
+
+    @Override
+    public int getRowCount() {
+        return questionarios.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return coluna.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        QuestionarioSocioeconomico obj = questionarios.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return obj.getDono().getNome();
+            case 1:
+                return obj.getDono().getCep();
+            default:
+                LOG.warning("Valor da coluna Invalido.\n"
+                        + "Provavelmente há colunas alem das necessarias para Exibição\n"
+                        + "");
+                return "Valor da coluna Invalido.\n"
+                        + "Provavelmente há colunas alem das necessarias para Exibição\n"
+                        + "";
+        }
+    }
+    private static final Logger LOG = Logger.getLogger(QuestionarioSocialTableModel.class.getName());
+
+}
