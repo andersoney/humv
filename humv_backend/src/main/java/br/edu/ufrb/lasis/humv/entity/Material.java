ffcfa9c69901df50faed25d821be3f0665d24869
Andersoney Rodrigues <andersoney@gmail.com>
2017-02-03 11:29:57 -0300

    Alterado Tipo de String para inteiro no servidor.

--- humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Material.java
+++ humv_backend/src/main/java/br/edu/ufrb/lasis/humv/entity/Material.java
@@ -11,54 +11,50 @@ import javax.persistence.Table;
 @Table(name = "MATERIAL")
 public class Material implements Serializable {
 
-	private static final long serialVersionUID = -6125748157292589614L;
+    private static final long serialVersionUID = -6125748157292589614L;
 
-	@Id
-	@GeneratedValue
-	private BigInteger id;
-	private String discriminacao;
-	private String unidade;
-	private String tipo; // material ou medicamento
-	private double valor; // em R$
+    @Id
+    @GeneratedValue
+    private BigInteger id;
+    private String discriminacao;
+    private String unidade;
+    private Integer tipo; // material ou medicamento
+    private double valor; // em R$
 
-	public BigInteger getId() {
-		return id;
-	}
+    public BigInteger getId() {
+        return id;
+    }
 
-	public void setId(BigInteger id) {
-		this.id = id;
-	}
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
 
-	public String getDiscriminacao() {
-		return discriminacao;
-	}
+    public String getDiscriminacao() {
+        return discriminacao;
+    }
 
-	public void setDiscriminacao(String discriminacao) {
-		this.discriminacao = discriminacao;
-	}
+    public void setDiscriminacao(String discriminacao) {
+        this.discriminacao = discriminacao;
+    }
 
-	public String getUnidade() {
-		return unidade;
-	}
+    public String getUnidade() {
+        return unidade;
+    }
 
-	public void setUnidade(String unidade) {
-		this.unidade = unidade;
-	}
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
 
-	public String getTipo() {
-		return tipo;
-	}
+    public Integer getTipo() {
+        return tipo;
+    }
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
 
-	public double getValor() {
-		return valor;
-	}
-
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 
 }
