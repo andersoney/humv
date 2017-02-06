0d411f9b958bc06f522d5e96b8fe9d7c7af6343e
Luiz Toni <luiztoni@outlook.com>
2017-02-06 14:01:54 -0300

    Adicionando opção de buscar Questionário por NIS ao serviço

--- humv_backend/src/main/java/br/edu/ufrb/lasis/humv/dao/QuestionarioSocioeconomicoDAO.java
+++ humv_backend/src/main/java/br/edu/ufrb/lasis/humv/dao/QuestionarioSocioeconomicoDAO.java
@@ -11,45 +11,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import br.edu.ufrb.lasis.humv.entity.QuestionarioSocioeconomico;
+import br.edu.ufrb.lasis.humv.utils.NumberUtils;
 
 @Repository
 public class QuestionarioSocioeconomicoDAO extends GenericDAO<QuestionarioSocioeconomico> implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Autowired
-    private SessionFactory sessionFactory;
+	@Autowired
+	private SessionFactory sessionFactory;
 
-    @Override
-    public Session getSession() {
-        return sessionFactory.getCurrentSession();
-    }
+	@Override
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
 
-    @Transactional
-    public void saveQuestionario(QuestionarioSocioeconomico questionario) {
-        super.save(questionario);
-    }
+	@Transactional
+	public void saveQuestionario(QuestionarioSocioeconomico questionario) {
+		super.save(questionario);
+	}
 
-    @Transactional
-    public void updateQuestionario(QuestionarioSocioeconomico questionario) {
-        super.update(questionario);
-    }
+	@Transactional
+	public void updateQuestionario(QuestionarioSocioeconomico questionario) {
+		super.update(questionario);
+	}
 
-    @Transactional
-    public void removeQuestionario(QuestionarioSocioeconomico questionario) {
-        super.delete(questionario);
-    }
+	@Transactional
+	public void removeQuestionario(QuestionarioSocioeconomico questionario) {
+		super.delete(questionario);
+	}
 
-    @Transactional
-    public QuestionarioSocioeconomico findByKey(BigInteger id) {
-        return (QuestionarioSocioeconomico) getCriteria().add(Restrictions.eq("id", id)).uniqueResult();
-    }
+	@Transactional
+	public QuestionarioSocioeconomico findByKey(BigInteger id) {
+		return (QuestionarioSocioeconomico) getCriteria().add(Restrictions.eq("id", id)).uniqueResult();
+	}
 
-    @SuppressWarnings("unchecked")
-    @Transactional
-    public List<QuestionarioSocioeconomico> search(String palavrachave) {
-    	Criteria criteria = getCriteria().createAlias("dono", "d").add(Restrictions.ilike("d.nome", "%" + palavrachave + "%"));
-        return criteria.list();
-    }
+	@SuppressWarnings("unchecked")
+	@Transactional
+	public List<QuestionarioSocioeconomico> search(String palavrachave) {
+		Criteria criteria = getCriteria();
+		BigInteger conversionResult = NumberUtils.convertStringToBigInteger(palavrachave);
+		if (conversionResult != null) {
+			criteria.add(Restrictions.eq("nis", conversionResult));
+		} else {
+			criteria = getCriteria().createAlias("dono", "d")
+					.add(Restrictions.ilike("d.nome", "%" + palavrachave + "%"));
+		}
+		return criteria.list();
+	}
 
 }
